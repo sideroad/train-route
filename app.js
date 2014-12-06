316e16d2a54a7b8de34423e3c3b9db1fee73cc0b
sideroad <sideroad.jp@gmail.com>
2014-12-06 21:44:38 +0900

    Change redis settings

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var redis = require('redis');
-var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
 
 var app = express();
 
