d6a27c52294b9dd6780ddabb1087047d37853849
sideroad <sideroad.jp@gmail.com>
2014-12-06 22:04:34 +0900

    Fixed parse bugs

--- routes/index.js
+++ routes/index.js
@@ -5,83 +5,143 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+var superagent = require('superagent');
+var async = require('async');
+client.auth(redisURL.auth.split(":")[1]); 
+client.flushdb();
 
-/* GET home page. */
-router.get('/route/:from/:to/:date/:time/:type/', function(req, res) {
-  var url = 'http://transit.loco.yahoo.co.jp/search/result?'+
-            'from='+req.params.from+'&'+
-            'to='+req.params.to+'&'+
-            'ym='+req.params.date.match(/^(\d\d\d\d\d\d)\d\d$/)[1]+'&'+
-            'd=' +req.params.date.match(/^\d\d\d\d\d\d(\d\d)$/)[1]+'&'+
-            'hh='+req.params.time.match(/^(\d\d)\d\d$/)[1]+'&'+
-            'm1='+req.params.time.match(/^\d\d\d(\d)$/)[1]+'&'+
-            'm2='+req.params.time.match(/^\d\d(\d)\d$/)[1]+'&'+
-            'type=1&'+
-            'ticket=ic&'+
-            's='+req.params.type+'&'+ // 0: First, 1: Price, 2: Ride count 
-            'expkind=1&'+
-            'ws=1';
+router.get('/nearest/:lat/:lng', function(req, res){
+  try {
+    async.waterfall([
+      function(callback){
+        var url = 'https://maps.googleapis.com/maps/api/place/search/json?'+
+                  'location='+req.params.lat+','+req.params.lng+'&'+
+                  'types=subway_station|train_station&'+
+                  'sensor=true&'+
+                  'rankby=distance&'+
+                  'language=ja&'+
+                  'key='+process.env.GOOGLE_API_KEY;
+        console.log(url);
 
+        superagent.get(url, function(place){
+          callback(null, place.body.results[0] || {});
+        });
+      },
+      function(place, callback){
+        var url = 'https://maps.googleapis.com/maps/api/directions/json?'+
+                  'origin='+req.params.lat+','+req.params.lng+'&'+
+                  'destination='+place.geometry.location.lat+','+place.geometry.location.lng+'&'+
+                  'sensor=false&'+
+                  'mode=walking&'+
+                  'key='+process.env.GOOGLE_API_KEY;
 
-client.get(url, function (err, routes) {
-  if(routes) {
-    res.send({
-      routes: JSON.parse(routes)
-    });
-  } else {
-    jsdom.env(
-      url,
-      ['http://code.jquery.com/jquery.js'],
-      function (errors, window) {
-        var $ = window.$,
-            routes = [{},{},{}];
-
-        $('#route01, #route02, #route03').each(function(index){
-          var $route = $(this);
-
-          $route.find('li.time').each(function(){
-            var str = $(this).text(),
-                matched = str.match(/^(\d\d\:\d\d)発→(\d\d\:\d\d)着(\d+時間)?(\d+分)（乗車(\d+)分）$/);
-
-            routes[index].departure = matched[1];
-            routes[index].arrival = matched[2];
-            routes[index].min = ( ( Number( (matched[3]||'').replace(/時間/,'') ) * 60 )+
-                               ( Number( (matched[4]||'').replace(/分/,'') )) );
+        console.log(url);
+        superagent.get(url, function(directions){
+          var duration = directions.body.routes[0].legs.reduce(function(sum, leg){
+            return sum + leg.duration.value;
+          },0);
+          callback(null, {
+            name: place.name.split(/\s+/).pop(),
+            duration: duration
           });
-
-          routes[index].stations = $route.find('.station').map(function(stationIndex){
-            var $station = $(this),
-                $times = $station.find('ul.time li'),
-                hasDeparture = $times.length === 1 && stationIndex !== 0 ? false : true,
-                hasArrival   = stationIndex === 0 ? false : true;
-
-            return {
-              station:   $station.find('dt').text(),
-              departure: hasDeparture ? $times.filter(':last-child').text().replace(/発|着/,'') : '',
-              arrival:   hasArrival ?   $times.filter(':first-child').text().replace(/発|着/,'') : ''
-            };
-          }).get();
-
-          routes[index].access = $route.find('.access').map(function(accessIndex){
-            var $access = $(this);
-
-            return {
-              line:   $access.find('.transport div').text().replace(/(\n|\[train\])/g, ''),
-              steps:  $access.find('.btnStopNum').text().replace(/駅/g, '')
-            };
-          }).get();
-        });
-
-        client.set( url, JSON.stringify(routes) );
-        res.send({
-          routes: routes
-        });
+        });          
       }
-    );
+    ], function(err, json){
+        res.json(json);
+    });
+  } catch (err){
+    console.log(err);
   }
-});
 
+
+
+});
+router.get('/route/:from/:to/:date/:time/:sort/:type/', function(req, res) {  
+  try {
+    var url = 'http://transit.loco.yahoo.co.jp/search/result?'+
+              'from='+req.params.from+'&'+
+              'to='+req.params.to+'&'+
+              'ym='+req.params.date.match(/^(\d\d\d\d\d\d)\d\d$/)[1]+'&'+
+              'd=' +req.params.date.match(/^\d\d\d\d\d\d(\d\d)$/)[1]+'&'+
+              'hh='+req.params.time.match(/^(\d\d)\d\d$/)[1]+'&'+
+              'm1='+req.params.time.match(/^\d\d(\d)\d$/)[1]+'&'+
+              'm2='+req.params.time.match(/^\d\d\d(\d)$/)[1]+'&'+
+              'type='+req.params.type+'&'+ // 1: Departure time 2: Last train
+              'ticket=ic&'+
+              's='+req.params.sort+'&'+ // 0: First, 1: Price, 2: Ride count 
+              'expkind=1&'+
+              'ws=1';
+
+    console.log(url);
+
+    res.set({
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'POST, GET, OPTIONS',
+      'Access-Control-Allow-Headers':'*'
+    });
+
+    client.get(url, function (err, routes) {
+      if(routes) {
+        res.json({
+          routes: JSON.parse(routes)
+        });
+      } else {
+        jsdom.env(
+          url,
+          ['http://code.jquery.com/jquery.js'],
+          function (errors, window) {
+            var $ = window.$,
+                routes = [{},{},{}];
+
+            $('#route01, #route02, #route03').each(function(index){
+              var $route = $(this);
+
+              $route.find('li.time').each(function(){
+                var str = $(this).text(),
+                    matched = str.match(/^(\d\d\:\d\d)発→(\d\d\:\d\d)着(\d+時間)?(\d+分)（乗車(\d+時間)?(\d+)分）$/);
+
+                routes[index].departure = matched[1];
+                routes[index].arrival = matched[2];
+                routes[index].min = ( ( Number( (matched[3]||'').replace(/時間/,'') ) * 60 )+
+                                   ( Number( (matched[4]||'').replace(/分/,'') )) );
+              });
+
+              routes[index].stations = $route.find('.station').map(function(stationIndex){
+                var $station = $(this),
+                    $times = $station.find('ul.time li'),
+                    hasDeparture = $times.length === 1 && stationIndex !== 0 ? false : true,
+                    hasArrival   = stationIndex === 0 ? false : true;
+
+                return {
+                  station:   $station.find('dt').text(),
+                  departure: hasDeparture ? $times.filter(':last-child').text().replace(/発|着/,'') : '',
+                  arrival:   hasArrival ?   $times.filter(':first-child').text().replace(/発|着/,'') : ''
+                };
+              }).get();
+
+              routes[index].access = $route.find('.access').map(function(accessIndex){
+                var $access = $(this),
+                    approach = $access.find('.transport div').text();
+
+                return {
+                  approach: approach.match(/\[([^\]]+)\]/)[1],
+                  line:   /\[train\]/.test(approach) ? approach.replace(/(\n|\[train\])/g, '') : '',
+                  steps:  Number($access.find('.btnStopNum').text().replace(/駅/g, ''))||1
+                };
+              }).get();
+            });
+
+            client.set( url, JSON.stringify(routes) );
+            res.json({
+              routes: routes
+            });
+          }
+        );
+      }
+    });
+  } catch(err){
+    console.log(err);
+  }
 
 });
 
