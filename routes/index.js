9a040f553aede281149c33012d175642504ef5b3
sideroad <sideroad.jp@gmail.com>
2014-12-17 22:12:51 +0900

    Get 9 routes

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,9 @@ var superagent = require('superagent');
 var async = require('async');
 var _ = require('lodash');
 var ignore = [
-  'ChIJ93NBFRh9GGARjH88N0VO3ms', //ベイサイドステーション駅・舞浜リゾートライン／ディズニーリゾートラ
   'ChIJd3RBFRh9GGARZe607wjJjJM', //ベイサイド・ステーション駅
   'ChIJ7-y9RhR9GGARwgKYxSeXAtY', //東京ディズニーランド・ステーション駅
-  'ChIJyZK9RhR9GGARxhCU3-o_rWY', //東京ディズニーランドステーション駅・舞浜リゾートライン／ディズニーリゾートライン
   'ChIJ41TW0BF9GGARxRxNrNtkbQ8', //リゾートゲートウェイ・ステーション駅
-  'ChIJm1TW0BF9GGARiGwkskKVafg', //リゾートゲートウェイステーション駅・舞浜リゾートライン／ディズニーリゾートライン  
 ];
 var get = function(url, callback){
   console.log(url);
@@ -99,81 +96,106 @@ router.get('/route/:from/:to/:date/:time/:sort/:type/', function(req, res) {
               'ticket=ic&'+
               's='+req.params.sort+'&'+ // 0: First, 1: Price, 2: Ride count 
               'expkind=1&'+
-              'ws=1';
+              'ws=1',
+        parse = function(url, callback){
+                  client.get(url, function (err, routes) {
+                    if(routes) {
+                      res.json({
+                        routes: JSON.parse(routes)
+                      });
+                    } else {
+                      jsdom.env(
+                        url,
+                        ['http://code.jquery.com/jquery.js'],
+                        function (errors, window) {
+                          var $ = window.$,
+                              routes = [{},{},{}],
+                              nextUrl;
+
+                          $('#route01, #route02, #route03').each(function(index){
+                            var $route = $(this);
+
+                            $route.find('li.time').each(function(){
+                              var str = $(this).text(),
+                                  matched = str.match(/^(\d\d\:\d\d)発→(\d\d\:\d\d)着(\d+時間)?(\d+分)（乗車(\d+時間)?(\d+)分）$/);
+
+                              routes[index].departure = matched[1];
+                              routes[index].arrival = matched[2];
+                              routes[index].min = ( ( Number( (matched[3]||'').replace(/時間/,'') ) * 60 )+
+                                                 ( Number( (matched[4]||'').replace(/分/,'') )) );
+                            });
+
+                            routes[index].stations = $route.find('.station').map(function(stationIndex){
+                              var $station = $(this),
+                                  $times = $station.find('ul.time li'),
+                                  hasDeparture = $times.length === 1 && stationIndex !== 0 ? false : true,
+                                  hasArrival   = stationIndex === 0 ? false : true;
+
+                              return {
+                                station:   $station.find('dt').text(),
+                                departure: hasDeparture ? $times.filter(':last-child').text().replace(/発|着/,'') : '',
+                                arrival:   hasArrival ?   $times.filter(':first-child').text().replace(/発|着/,'') : ''
+                              };
+                            }).get();
+
+                            routes[index].access = $route.find('.access').map(function(accessIndex){
+                              var $access = $(this),
+                                  approach = $access.find('.transport div').text();
+
+                              return {
+                                approach: approach.match(/\[([^\]]+)\]/)[1],
+                                line:   /\[train\]/.test(approach) ? approach.replace(/(\n|\[train\])/g, '') : '',
+                                steps:  Number($access.find('.btnStopNum').text().replace(/駅/g, ''))||1
+                              };
+                            }).get();
+
+                            var md5 = crypto.createHash('md5');
+                            md5.update(JSON.stringify(routes),'utf8');
+                            routes[index].id = md5.digest('hex');
+                          });
+
+                          nextUrl = $('.navSearchAround li.next a').attr('href');
+
+                          client.set( url, JSON.stringify(routes) );
+                          client.expire( url, 86400000);
+                          callback(null, routes, nextUrl);
+                        }
+                      );
+                    }
+                  });
+                };
 
     console.log(url);
 
-    client.get(url, function (err, routes) {
-      if(routes) {
-        res.json({
-          routes: JSON.parse(routes)
+    async.waterfall([
+      function(callback){
+        parse(url, function(routes, nextUrl){
+          callback(null, routes, nextUrl);
         });
-      } else {
-        jsdom.env(
-          url,
-          ['http://code.jquery.com/jquery.js'],
-          function (errors, window) {
-            var $ = window.$,
-                routes = [{},{},{}];
+      },
+      function(routes, url, callback){
+        parse(url, function(_routes, nextUrl){
+          callback(null, routes.concat(_routes), nextUrl);
+        });
+      },
+      function(routes, url, callback){
+        parse(url, function(_routes, nextUrl){
+          callback(null, routes.concat(_routes), nextUrl);
+        });
+      },
+    ], function(routes, nextUrl){
+      res.set({
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers':'*'
+      });
 
-            $('#route01, #route02, #route03').each(function(index){
-              var $route = $(this);
-
-              $route.find('li.time').each(function(){
-                var str = $(this).text(),
-                    matched = str.match(/^(\d\d\:\d\d)発→(\d\d\:\d\d)着(\d+時間)?(\d+分)（乗車(\d+時間)?(\d+)分）$/);
-
-                routes[index].departure = matched[1];
-                routes[index].arrival = matched[2];
-                routes[index].min = ( ( Number( (matched[3]||'').replace(/時間/,'') ) * 60 )+
-                                   ( Number( (matched[4]||'').replace(/分/,'') )) );
-              });
-
-              routes[index].stations = $route.find('.station').map(function(stationIndex){
-                var $station = $(this),
-                    $times = $station.find('ul.time li'),
-                    hasDeparture = $times.length === 1 && stationIndex !== 0 ? false : true,
-                    hasArrival   = stationIndex === 0 ? false : true;
-
-                return {
-                  station:   $station.find('dt').text(),
-                  departure: hasDeparture ? $times.filter(':last-child').text().replace(/発|着/,'') : '',
-                  arrival:   hasArrival ?   $times.filter(':first-child').text().replace(/発|着/,'') : ''
-                };
-              }).get();
-
-              routes[index].access = $route.find('.access').map(function(accessIndex){
-                var $access = $(this),
-                    approach = $access.find('.transport div').text();
-
-                return {
-                  approach: approach.match(/\[([^\]]+)\]/)[1],
-                  line:   /\[train\]/.test(approach) ? approach.replace(/(\n|\[train\])/g, '') : '',
-                  steps:  Number($access.find('.btnStopNum').text().replace(/駅/g, ''))||1
-                };
-              }).get();
-
-              var md5 = crypto.createHash('md5');
-              md5.update(JSON.stringify(routes),'utf8');
-              routes[index].id = md5.digest('hex');
-            });
-
-            client.set( url, JSON.stringify(routes) );
-            client.expire( url, 86400000);
-            res.set({
-              'Access-Control-Allow-Origin':'*',
-              'Access-Control-Allow-Methods':'POST, GET, OPTIONS',
-              'Access-Control-Allow-Headers':'*'
-            });
-
-            res.json({
-              routes: routes
-            });
-            res.end();
-          }
-        );
-      }
+      res.json({
+        routes: routes
+      });
+      res.end();
     });
+
 
   } catch(err){
     console.log(err);
